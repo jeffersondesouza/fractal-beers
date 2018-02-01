f9240a4dfcd281d278dae261afa7d9b895cc1c40
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-02-01 21:33:00 -0200

    call to acytoon ok

--- webapp/src/components/Header/HeaderNav.js
+++ webapp/src/components/Header/HeaderNav.js
@@ -9,8 +9,8 @@ export default class HeaderNav extends Component {
       <nav className="header__nav">
         <ul className="header__nav__links">
           <li><Link className="header__nav__link secondary-color" to={`/`}>Início</Link></li>
-          <li><Link className="header__nav__link secondary-color" to={`/beers`}>Loja</Link></li>
-          <li><Link className="header__nav__link secondary-color" to={`/about`}>Sobre</Link></li>
+          <li><Link className="header__nav__link secondary-color" to={`/beers`}>Cervejas</Link></li>
+          {/* <li><Link className="header__nav__link secondary-color" to={`/about`}>Sobre</Link></li> */}
         </ul>
       </nav>
     );
--- webapp/src/components/Home/HomePresentation.js
+++ webapp/src/components/Home/HomePresentation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 import './scss/home-presetation.scss';
 
@@ -7,9 +8,10 @@ export default class HomePresentation extends Component {
   render() {
     return (
       <section className="home__presetation">
-        <p className="home__presetation__tagline">
-          Bons momentos combinam com boas Cervejas</p>
-        <button className="home__presetation__call-to-action">Escolha a sua</button>
+        <p className="home__presetation__tagline">Bons momentos combinam com boas Cervejas</p>
+        <div className="home__presetation__button">
+          <Link to={'/beers'} className="home__presetation__call-to-action">Escolha a sua</Link>
+        </div>
       </section>
     );
   }
