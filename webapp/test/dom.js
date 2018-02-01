f5dde87620b81b24d2aeab5d331b5c30612356a8
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-02-01 08:28:01 -0200

    test env ok

---
+++ webapp/test/dom.js
@@ -0,0 +1,26 @@
+/* const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+
+global.window = document.defaultView;
+
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+}; */
+
+
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const { document } = (new JSDOM('')).window;
+global.document = document;
---
+++ webapp/test/tests/App-component/App.spec.js
@@ -0,0 +1,15 @@
+describe('a passing test', () => {
+  it('should pass', () => {
+    expect(true).to.be.true;
+  });
+
+  it('should sdd 2 + 2', () => {
+    expect(2+2).to.be.equal(4)
+  });
+
+
+  it('should sdd 2 + 2', () => {
+    expect(2+2).to.be.equal(4)
+  });
+
+});
