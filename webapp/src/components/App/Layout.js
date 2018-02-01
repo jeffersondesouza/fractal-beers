4097c8314fa75ccf74367df237c08d3ce79739b2
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-02-01 00:45:58 -0200

    basic layput ok

--- webapp/src/components/App/Layout.js
+++ webapp/src/components/App/Layout.js
@@ -6,19 +6,17 @@ export default class Layout extends Component {
     return (
 
 
-
-
       <div id="main">
         <header className="Header">
           <div className="container">
-            <figure className="Header-logo">
+            <figure className="Header-logo inline-block">
               <img className="img-responsive" src="src/images/logo.png" />
               <figcaption className="Header-logo-figcaption">
                 <span>Fractal</span>
                 <span className="Header-logo-figcaption--primary">Cervas</span>
               </figcaption>
             </figure>
-            <nav className="Header-nav">
+            <nav className="Header-nav inline-block">
               <ul className="Header-nav-links">
                 <li><a className="Header-nav-link" href="#">Início</a></li>
                 <li><a className="Header-nav-link" href="#">Loja</a></li>
@@ -29,12 +27,15 @@ export default class Layout extends Component {
           </div>
         </header>
         <main className="HomePage">
-          <figure className="HomePage-botle">
-            <img className="img-responsive" src="src/images/botle.png" />
-          </figure>
-          <p className="HomePage-tagline">Bons momentos combinam com boas Cervejas</p>
-          <button className="HomePage-callToAction">Escolha a sua</button>
-
+          <div className="container center">
+            <figure className="HomePage-botle inline-block">
+              <img src="src/images/botle.png" />
+            </figure>
+            <aside className="HomePage-action inline-block">
+              <p className="HomePage-tagline">Bons momentos combinam com boas Cervejas</p>
+              <button className="HomePage-button--callToAction">Escolha a sua</button>
+            </aside>
+          </div>
         </main>
         <footer>
           footer
